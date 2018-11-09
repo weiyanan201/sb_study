6c8c396ec39f97ed020226df0d4d46c510a57e8a
魏亚楠 <weiyanan.yannis@shandagames.com>
2018-11-09 19:21:45 +0800

    init

---
+++ src/main/java/com/wei/SbStudyApplication.java
@@ -0,0 +1,12 @@
+package com.wei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbStudyApplication.class, args);
+	}
+}
